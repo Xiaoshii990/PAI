e32e5fc3f8f17e2b02abf55cab6831c13b7fea76
Xiaoshii990 <kapek990@gmail.com>
2022-09-13 11:05:39 +0200

    Nic

---
+++ Lekcja 1/gjkg.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<meta charset="UTF-8">
+<title>
+Strona
+</title>
+
+<body>
+<center>
+"Hello World"
+</center>
+<?php
+$zmienna1 = "My counter 1";
+echo "Hello World","<br>". $zmienna1. "<br>", "Hello friend";
+?>
+</body>
+</head>
+</html>
+
